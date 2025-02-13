308c3b69b678e0ab6467c0e7c38e50b32e6ea360
0x90 <134144784+riccio8@users.noreply.github.com>
2025-02-13 18:12:53 +0100

    Update l298N.hpp

--- lib/l298N.hpp
+++ lib/l298N.hpp
@@ -1,44 +1,45 @@
-struct MotorPins {
+struct MotorPins { // struct for storing motor pin numbers so u don't have to repeat every pin for each motor
     int enable;
     int in1;
     int in2;
 };
 
 class Motor {
-private:
+private:        // private variables where u store the pin numbers for each motor
     MotorPins motorA;
     MotorPins motorB;
 
 public:
-    Motor(MotorPins _motorA, MotorPins _motorB);
+    Motor(MotorPins _motorA, MotorPins _motorB); // constructor for the motor class
 
-    void clockwiseA();
-    void counterClockwiseA();
+    void clockwiseA();      // functions for rotating the motors
+    void counterClockwiseA();   // functions for rotating the motors counter-clockwise
     
     void clockwiseB();
     void counterClockwiseB();
 
-    void enableA();
-    void disableA();
+    void enableA();     // functions for enabling the motor
+    void disableA();   // functions for disabling the motor
 
     void enableB();
     void disableB();
 
-    void disable() {
+    void disable() {  // function for disabling both motors
         disableA();
         disableB();
     }
 
-    void enable() {
+    void enable() { // function for enabling both motors
         enableA();
         enableB();
     }
 };
 
 Motor::Motor(MotorPins _motorA, MotorPins _motorB) {
-    motorA = _motorA;
+    motorA = _motorA; // store the pin numbers for each motor in the private variables
     motorB = _motorB;
 
+    // initialize the motor pins as outputs
     pinMode(motorA.enable, OUTPUT);
     pinMode(motorA.in1, OUTPUT);
     pinMode(motorA.in2, OUTPUT);
@@ -92,8 +93,8 @@ void Motor::disableB() {
 
 USAGE EXAMPLE:
 
-MotorPins motor1 = {5, 6, 7};  // Pin per il motore A
-MotorPins motor2 = {8, 9, 10}; // Pin per il motore B
+MotorPins motor1 = {5, 6, 7};  // Pin for motor A
+MotorPins motor2 = {8, 9, 10}; // Pin for motor B
 
 Motor myMotors(motor1, motor2);
 
@@ -115,8 +116,7 @@ void loop() {
 }
 
 
-MORE INFO AT:
-https://www.handsontec.com/dataspecs/L298N%20Motor%20Driver.pdf
+
 
 
 */
